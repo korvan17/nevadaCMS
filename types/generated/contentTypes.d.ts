55aa9c93afc70da2b627c26324802e4d342a897a
Alexandr Neveroff <neveroff1976@gmail.com>
2023-11-01 19:42:53 +0200

    strapi version updated to 4.15.0

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -23,6 +23,7 @@ export interface AdminPermission extends Schema.CollectionType {
       Attribute.SetMinMaxLength<{
         minLength: 1;
       }>;
+    actionParameters: Attribute.JSON & Attribute.DefaultTo<{}>;
     subject: Attribute.String &
       Attribute.SetMinMaxLength<{
         minLength: 1;
@@ -875,7 +876,7 @@ export interface ApiQuestionQuestion extends Schema.CollectionType {
   };
 }
 
-declare module '@strapi/strapi' {
+declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
       'admin::permission': AdminPermission;
